404e96f3f646542fa541fa9a91adeaf8916f86f1
Andy Block <ablock91@gmail.com>
2020-12-23 12:21:17 -0500

    removed the current year from get_todays_date() as it is unused

--- helper_files/get_todays_date.py
+++ helper_files/get_todays_date.py
@@ -11,6 +11,5 @@ def get_todays_date():
     
     current_day = todays_date.split('-')[2]
     current_month= todays_date.split('-')[1]
-    current_year= todays_date.split('-')[0]
     
-    return current_day, current_month, current_year
+    return current_day, current_month
